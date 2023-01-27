5dcf163e30fe3cb06f25d4216ae2de4a9523602b
Ash <59657132+sillyash@users.noreply.github.com>
2023-01-27 17:37:13 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import pyxel
+
+class App:
+    def __init__(self):
+        pyxel.init(1920, 1080,title="Sniper",fps=60,quit_key=pyxel.KEY_NONE,display_scale=4)
+        self.x = 0
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        self.x +=1
+
+    def draw(self):
+        pyxel.cls(0)
+
+App()
