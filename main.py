63ec2113453da8cdffd6e67af2f82097fefad9ab
Ash <59657132+sillyash@users.noreply.github.com>
2023-01-27 17:12:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import pyxel
+
+class App:
+    def __init__(self):
+        pyxel.init(1920, 1080,"Sniper",60,,4)
+        self.x = 0
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        self.x = (self.x + 1) % pyxel.width
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.rect(self.x, 0, 8, 8, 9)
+
+App()
