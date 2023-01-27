dcbd217e65f8fee603f9ff28c1e66281abc33ad6
Ash <59657132+sillyash@users.noreply.github.com>
2023-01-27 17:13:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pyxel
 
 class App:
     def __init__(self):
-        pyxel.init(1920, 1080,"Sniper",60,,4)
+        pyxel.init(1920, 1080,title="Sniper"fps=,60,quit_key=pyxel.KEY_NONE,display_scale=4)
         self.x = 0
         pyxel.run(self.update, self.draw)
 
